15a821b9d374cd94b3b2ba77f49f6fc59383cd43
Jack M <exegete@mac.com>
2022-01-17 11:46:58 -0700

    rails new w/ uuid.

---
+++ config/initializers/generators.rb
@@ -0,0 +1,5 @@
+# config/initializers/generators.rb
+# Set Postgres to use UUID for the default keys in the database.
+Rails.application.config.generators do |g|
+  g.orm :active_record, primary_key_type: :uuid
+end
